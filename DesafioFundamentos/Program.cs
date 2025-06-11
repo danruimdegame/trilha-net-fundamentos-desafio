7cd9bb86748ead34d8abc1caa2838608d4acde81
danruimdegame <danieltomaz1931@gmail.com>
2025-06-10 22:56:20 -0300

    feat: Adiciona uma logo à interface do usuário

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,8 +6,16 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+string logo = @" 
+   __    _             _                                        _         
+  /__\__| |_ __ _  ___(_) ___  _ __   __ _ _ __ ___   ___ _ __ | |_ ___   
+ /_\/ __| __/ _` |/ __| |/ _ \| '_ \ / _` | '_ ` _ \ / _ \ '_ \| __/ _ \  
+//__\__ \ || (_| | (__| | (_) | | | | (_| | | | | | |  __/ | | | || (_) | 
+\__/|___/\__\__,_|\___|_|\___/|_| |_|\__,_|_| |_| |_|\___|_| |_|\__\___/  
+                                                                          
+";
+
+Console.WriteLine($"{logo}\n" + "Seja bem vindo ao sistema de estacionamento!\n" + "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
@@ -16,13 +24,14 @@ precoPorHora = Convert.ToDecimal(Console.ReadLine());
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
-string opcao = string.Empty;
+
 bool exibirMenu = true;
 
 // Realiza o loop do menu
 while (exibirMenu)
 {
     Console.Clear();
+    Console.WriteLine($"{logo}");
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
